85b6d3fc4ba53537966a1699e84f71bf32029da3
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-09-25 20:50:56 +0300

    static files added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const initHadlebars = require('./config/handlebars.js');
 
 const app = express();
 
 initHadlebars(app);
+
+app.use(express.static(path.resolve(__dirname, './public')));
 
 app.all('/', (req, res) => {
     res.render('index');

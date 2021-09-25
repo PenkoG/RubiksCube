1a8bbc90f1d8c0f3afc93189a8e1fe48592b5e70
PenkoG <81184142+PenkoG@users.noreply.github.com>
2021-09-25 20:17:09 +0300

    added view engine

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
+app.set('views', path.resolve(__dirname, './views'))
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+
 app.all('/', (req, res) => {
-    res.write('Working');
-    res.end;
+    res.render('index', {layout: false});
 })
 
 app.listen(5000, () => {
